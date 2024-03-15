6960a1dad09502e24954460f7e9b2ad12da95d56
Ellaluvscheese <gal21029@byui.edu>
2024-03-14 23:32:18 -0600

    Co-authored-by: Kathryn Lambert <kathrynruth8@users.noreply.github.com> Co-authored-by: brinleylott <brinleylott@users.noreply.github.com> Co-authored-by: Sergio <Sergioo29@users.noreply.github.com>

---
+++ grandpas-kitchen/src/js/components/Home.svelte
@@ -0,0 +1,12 @@
+<p>Home</p>
+
+<div class="aboutUs">
+    <h3>About Us</h3>
+    <p>
+        Grandpa's Kitchen is a digital gathering place for sharing and discovering family recipes from around the globe.<br>
+        This free platform combines nostalgia with a user-friendly interface, inviting cooks of all levels to explore, save, 
+        and contribute to a growing collection of dishes. <br> Emphasizing simplicity and the joy of home cooking, it fosters a 
+        community where culinary traditions are preserved and new ones are born. Grandpa's Kitchen is more than just a recipe site; 
+        it's a celebration of family, heritage, and the universal love of food.
+    </p>
+</div>
