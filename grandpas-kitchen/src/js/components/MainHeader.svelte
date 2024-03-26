cf642a7a0911267aa718797534cdad8b338a0efe
brinleylott <bvg56010@gmail.com>
2024-03-26 13:30:19 -0600

    most of the css for header and footer

--- grandpas-kitchen/src/js/components/MainHeader.svelte
+++ grandpas-kitchen/src/js/components/MainHeader.svelte
@@ -1,9 +1,54 @@
 <header>
-    <h1>Grandpa's Kitchen</h1>
-    <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#recipes">Recipes</a></li>
-        <li><a href="#profile">Profile</a></li>
-    </ul>
-    <button>Create</button>
+    <div class="headerDiv">
+        <h1>Grandpa's Kitchen</h1>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#recipes">Recipes</a></li>
+            <li><a href="#profile">Profile</a></li>
+        </ul>
+        <button>Create</button>
+    </div>
+    
 </header>
+
+<style>
+    .headerDiv {
+        background-color: #F6E6D6;
+        display: flex;
+        justify-content: space-between;
+    }
+    h1 {
+        font-size: 30px;
+        color: #283D64;
+        padding-left: 10px;
+    }
+    ul {
+        display: flex;
+        padding-left: 5px;
+        list-style-type: none;
+        margin-right: 200px;
+    }
+    a {
+        color: #283D64;
+        text-decoration: none;
+        margin-top: 5px;
+    }
+    li {
+        display: flex;
+        justify-content: space-between;
+        padding: 10px;  
+    }
+    button {
+        padding: 0;
+        height: 30px;
+        width: 75px;
+        background-color: #D15220;
+        color: #F6E6D6;
+        border-radius: 30px;
+        margin: auto;
+        border-width: 0;
+    }
+    #app {
+        padding: 0;
+    }
+</style>
