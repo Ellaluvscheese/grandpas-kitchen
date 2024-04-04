de4c1707f7d9d1d752bb014778661d954921d398
Sergio <sergiohds_29@hotmail.com>
2024-04-04 11:13:14 -0600

    added an alternate search bar

--- grandpas-kitchen/src/js/components/MainHeader.svelte
+++ grandpas-kitchen/src/js/components/MainHeader.svelte
@@ -6,18 +6,28 @@
             <li><a href="#profile">Profile</a></li>
             <li><a href="#search">Search Page</a></li>
         </ul>
-        <div class="searchSVG">
-            <svg version="1.1" id="Layer_3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="0 0 360 360" style="enable-background:new 0 0 360 360;" xml:space="preserve">
-                <style type="text/css">
-                    .st0{fill:none;stroke:#000000;stroke-width:15;stroke-miterlimit:10;}
-                    .st1{stroke:#000000;stroke-miterlimit:10;}
-                </style>
-                <circle class="st0" cx="137.1" cy="137.2" r="95.9"/>
-                <line class="st0" x1="200.5" y1="209.1" x2="311.5" y2="311.6"/>
-                <path class="st1" d="M316,306.2c0,0,5.3,4.3,1.6,9.7s-10.5,1.1-10.5,1.1L316,306.2L316,306.2z"/>
-            </svg>
+
+        <div class="searchContainer">
+            <form>
+                <input class="searchBar" placeholder="what are you looking for?"> 
+            </form>
+            <p>🔍</p>
+            
+            <!-- SVG FOR SEARCH ICON-->
+            <!-- <div class="searchSVG">
+                <svg version="1.1" id="Layer_3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    viewBox="0 0 360 360" style="enable-background:new 0 0 360 360;" xml:space="preserve">
+                    <style type="text/css">
+                        .st0{fill:none;stroke:#000000;stroke-width:15;stroke-miterlimit:10;}
+                        .st1{stroke:#000000;stroke-miterlimit:10;}
+                    </style>
+                    <circle class="st0" cx="137.1" cy="137.2" r="95.9"/>
+                    <line class="st0" x1="200.5" y1="209.1" x2="311.5" y2="311.6"/>
+                    <path class="st1" d="M316,306.2c0,0,5.3,4.3,1.6,9.7s-10.5,1.1-10.5,1.1L316,306.2L316,306.2z"/>
+                </svg>
+            </div> -->
         </div>
+        
         <button>Create</button>
     </div>
     
