c98e419e91a040dba6eddf41865dbe91f072580c
brostkyl <bro19089@byui.edu>
2024-04-03 23:16:31 -0600

    recipe details page and styles added

--- grandpas-kitchen/src/js/components/Recipes.svelte
+++ grandpas-kitchen/src/js/components/Recipes.svelte
@@ -1,37 +1,90 @@
 <script>
   import { getRecipeById } from "../externalServices.js";
-  import {onMount} from "svelte";
+  import { onMount } from "svelte";
   import { get } from "svelte/store";
 
   export let params = {};
-  let recipeDetails ={};
+  let recipeDetails = {};
 
- onMount (async () => {
-    const recipeId = params.get("id");
-    console.log(recipeId);
+  onMount(async () => {
+    const recipeId = params.get("recipe");
+    console.log("hello", recipeId);
     recipeDetails = await getRecipeById(recipeId);
- });
-  
+  });
+  console.log(recipeDetails);
 </script>
 
-<section>
-  <img
-    class="recipe-image"
-    src="data:image/jpeg;base64, {recipeDetails.recipeImage}"
-    alt="Image of {recipeDetails.name}"
-  />
-</section>
-<section>
-  <a href="/index.html?recipe={recipeDetails._id}">
-    <h2>{recipeDetails.name}</h2>
-    <p class="recipe-description">{recipeDetails.description}</p>
-    <p class="recipe-catagories">{recipeDetails.categories}</p>
-    <p class="recipe-cooktime">Cooktime: {recipeDetails.cookTime}</p>
-    <p class="recipe-preptime">| Preptime: {recipeDetails.prepTime}</p>
-    <p class="recipe-servings">Servings: {recipeDetails.servings}</p>
-    <p class="recipe-ingredients">Ingredients: {recipeDetails.ingredients}</p>
-    <p class="recipe-instructions">
-      Instructions: {recipeDetails.instructions}
-    </p>
-  </a>
-</section>
+{#if recipeDetails.name}
+  <section class="recipe-container">
+    <section class="image-section">
+      <img
+        class="recipe-image"
+        src="data:image/jpeg;base64, {recipeDetails.recipeImage}"
+        alt="Image of {recipeDetails.name}"
+      />
+    </section>
+    <section class="details-section">
+      <h2>{recipeDetails.name}</h2>
+      <p class="recipe-description">{recipeDetails.description}</p>
+      <p class="recipe-catagories">{recipeDetails.categories}</p>
+      <div class="recipe-info">
+        <p class="recipe-cooktime">
+          Cooktime: {recipeDetails.cookTime} min&nbsp
+        </p>
+        <p class="recipe-preptime">
+          | Preptime: {recipeDetails.prepTime} min&nbsp
+        </p>
+        <p class="recipe-servings">| Servings: {recipeDetails.servings}</p>
+      </div>
+      <p class="recipe-ingredients">Ingredients:</p>
+      <ul>
+        {#each recipeDetails.ingredients as ingredient}
+          <li>{ingredient.name}</li>
+        {/each}
+      </ul>
+      <p class="recipe-instructions">Instructions:</p>
+      <ol>
+        <li>{recipeDetails.instructions}</li>
+      </ol>
+    </section>
+  </section>
+{:else}
+  <p>Loading...</p>
+{/if}
+
+<style>
+  .recipe-container {
+    display: flex;
+  }
+  .image-section {
+    flex: 1;
+    margin-left: 15%;
+  }
+
+  .details-section {
+    flex: 2;
+    margin-left: 5%;
+    margin-right: 15%;
+  }
+  .recipe-image {
+    max-height: 550px;
+    border-radius: 30px;
+    margin-top: 20px;
+  }
+
+  .recipe-info {
+    display: flex;
+  }
+  .recipe-info p {
+    margin: 0;
+  }
+  h2 {
+    font-weight: 600;
+  }
+
+  .recipe-catagories {
+    background: lightgray;
+    padding: 10px;
+    border-radius: 10px;
+  }
+</style>
